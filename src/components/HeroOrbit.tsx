bb6e8a1003124a5ba77951f6174bddf056cf225e
Emilio del Hierro <delhierroemilio@gmail.com>
2025-01-16 15:07:14 -0500

    Hero realizado

---
+++ src/components/HeroOrbit.tsx
@@ -0,0 +1,20 @@
+import { PropsWithChildren } from "react";
+
+const HeroOrbit = ({
+  children,
+  size,
+  rotation,
+}: PropsWithChildren<{ size: number; rotation: number }>) => {
+  return (
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ">
+      <div
+        className="flex justify-start items-start"
+        style={{ height: `${size}px`, width: `${size}px`, rotate: `${rotation}deg` }}
+      >
+        <div className="inline-flex" style={{transform: `rotate(${rotation * -1}deg)`}}>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroOrbit;
